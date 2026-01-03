537847977e758988606fa9704e3e5fb56d23203b
Tilcanet User <tilcaracristian@gmail.com>
2026-01-03 20:04:32 -0300

    Fix CSRF: Add SECURE_PROXY_SSL_HEADER

--- backend_todavia/settings.py
+++ backend_todavia/settings.py
@@ -34,6 +34,8 @@ DEBUG = os.getenv('DJANGO_DEBUG', 'True') == 'True'
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '*').split(',')
 
 CSRF_TRUSTED_ORIGINS = ['https://todavia.tilcanet.com.ar']
+# Necessary for Nginx passing HTTPS
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 CORS_ALLOW_ALL_ORIGINS = DEBUG  # Allow all only in debug
 
